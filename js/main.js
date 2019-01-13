f0e1e77068157084dea30e3c89a5060389692b01
Joshua Usselman <usselmanjosh@gmail.com>
2019-01-12 23:28:48 -0800

    basic maths

--- js/main.js
+++ js/main.js
@@ -1,73 +1,19 @@
-// var joshua = {
-//   name: 'Joshua',
-//   yearOfBirth: 1985,
-//   job: 'teacher'
-// };
-
-// var Person = function(name, yearOfBirth, job) {
-//   this.name = name;
-//   this.yearOfBirth = yearOfBirth;
-//   this.job = job;
-//   // this.calculateAge = function() {
-//   //   console.log(2019 - this.yearOfBirth);
-//   // }
-// };
-
-// Person.prototype.calculateAge = function() {
-//   console.log(2019 - this.yearOfBirth);
-// };
-
-// var john = new Person('John', 1990, 'teacher');
-// // john.calculateAge();
-
-// var joshua = new Person('Joshua', 1985, 'bleacher');
-
-// john.calculateAge();
-// joshua.calculateAge();
-// // console.log(joshua);
-// // console.log(john);
-
-// var personProto = {
-//   calculateAge: function() {
-//     console.log(2019 - this.yearOfBirth);
-//   }
-// };
-
-// var juan = Object.create(personProto);
-// juan.name = 'Juan';
-// juan.yearOfBirth = '1980';
-// juan.job = 'Teacher';
-
-// var juanita = Object.create(personProto, {
-//   name: { value: 'Juanita' },
-//   yearOfBirth: { value: 1969 },
-//   job: { value: 'designer' }
-// });
-
-// var years = [1900, 1969, 1985, 2001, 2018];
-
-// function arrayCalc(arr, fn) {
-//   var arrRes = [];
-//   for (var i = 0; i < arr.length; i++) {
-//     arrRes.push(fn(arr[i]));
-//   }
-
-//   return arrRes;
-// }
-
-// function calcAge(el) {
-//   return 2019 - el;
-// }
-
-// var ages = arrayCalc(years, calcAge);
-
-// console.log(ages);
-
 // get the date //
-var now, year;
+var time, currentYear;
 
-now = new Date();
+time = new Date();
 
-year = now.getFullYear;
+currentYear = time.getFullYear();
 
-console.log(year);
+// console.log(year);
+
+// grab the p to display the diff. in years //
+
+var calcYears = function() {
+  var userInput = document.getElementById('yearInput').value;
+
+  var yearsDiff = currentYear - userInput;
+
+  document.getElementById('diffDisplay').innerHTML =
+    'This event happened ' + yearsDiff + ' years ago';
+};
